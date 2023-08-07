87a62318ff4eff8ba35cbbd3fef38fc4ee2b4f52
PoliJun <rivenloving@gmail.com>
2023-08-07 13:51:13 +0800

    '[excercises_code] bug fix: minIsland doesn't return correct result when encountering W'

--- excercises_code/minimum-island.js
+++ excercises_code/minimum-island.js
@@ -1,10 +1,37 @@
-const minIsland = (grid) =>{
-    for(let r=0; r<grid.length; r++){
-        for(let c=0; c<grid[0].length; c++){
-            exploreSize(grid,r,c,visited)
+const minIsland = (grid) => {
+    let minIsland = Number.POSITIVE_INFINITY;
+    const visited = new Set();
+    for (let r = 0; r < grid.length; r++) {
+        for (let c = 0; c < grid[0].length; c++) {
+            const size = exploreSize(grid, r, c, visited);
+            // When meet a "W", the size would be zero, then minIsland would be 0. 
+            //We shouldn't do comparison when size is zero
+            if (size != 0 && size < minIsland) minIsland = size;
         }
     }
-}
+    return minIsland;
+};
+
+const exploreSize = (grid, r, c, visited) => {
+    // let size = 1;
+    const rowInbounds = r >= 0 && r < grid.length;
+    const colInbounds = c >= 0 && c < grid[0].length;
+    if (!rowInbounds || !colInbounds) return 0;
+
+    if (grid[r][c] === "W") return 0;
+    const pos = r + "," + c;
+    if (visited.has(pos)) return 0;
+    visited.add(pos);
+
+    let size = 1;
+    size += exploreSize(grid, r + 1, c, visited);
+    size += exploreSize(grid, r - 1, c, visited);
+    size += exploreSize(grid, r, c + 1, visited);
+    size += exploreSize(grid, r, c - 1, visited);
+    // size += 1;
+
+    return size;
+};
 
 const grid = [
     ["W", "L", "W", "W", "W"],
@@ -14,3 +41,5 @@ const grid = [
     ["L", "W", "W", "L", "L"],
     ["L", "L", "W", "W", "W"],
 ];
+
+console.log(minIsland(grid));
